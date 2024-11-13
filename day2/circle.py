db60118d23da79c31458bf4991f523296d917786
Divya Krishna Kumar <117076907+divz-k@users.noreply.github.com>
2024-11-13 12:16:42 +0200

    Add files via upload

---
+++ day2/circle.py
@@ -0,0 +1,6 @@
+radiusC = 5
+
+areaC = 3.14* radiusC^2
+circumferenceC = 2 * 3.14 * radiusC
+
+print('A Circle with radius ', radiusC, 'has a circumference ', circumferenceC, 'and area ', areaC)
---
+++ day2/rectangle.py
@@ -0,0 +1,7 @@
+widthR = 5
+lengthR = 7
+
+areaR=widthR*lengthR
+circumferenceR=2*(widthR+lengthR)
+
+print('For a rectangle with width ', widthR, 'and length ', lengthR, 'the area is ', areaR, 'and circumference is ',circumferenceR)
